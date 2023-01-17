645dea2538cf60b80c6c3caad5ae02a6398cf262
jcollic <johan.collic@etudiant.univ-rennes1.fr>
2023-01-17 09:39:05 +0100

    User et Ticket + premiere methode JpaTest

--- src/main/java/jpa/JpaTest.java
+++ src/main/java/jpa/JpaTest.java
@@ -1,33 +1,66 @@
 package jpa;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import javax.persistence.PersistenceContext;
 
-public class JpaTest {
+import jpa.classMetier.Ticket;
+import jpa.classMetier.User;
 
+public class JpaTest {
+	private EntityManager manager;
+
+	public JpaTest(EntityManager manager) {
+		this.manager = manager;
+	}
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		EntityManagerFactory factory =   
+ 			 Persistence.createEntityManagerFactory("example");
+		EntityManager manager = factory.createEntityManager();
+		JpaTest test = new JpaTest(manager);
 
-		EntityManager manager = EntityManagerHelper.getEntityManager();
 		EntityTransaction tx = manager.getTransaction();
 		tx.begin();
-
-
 		try {
-
+			test.createUsers();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		tx.commit();
 
-
-		manager.close();
-		EntityManagerHelper.closeEntityManagerFactory();
-		//		factory.close();
+		test.listUsers();
+			
+   	 manager.close();
+		System.out.println(".. done");
 	}
 
 
+
+	private void createUsers() {
+		int numOfUsers = manager.createQuery("Select a From Users a", User.class).getResultList().size();
+		if (numOfUsers == 0) {
+			Ticket ticket = new Ticket();
+			manager.persist(ticket);
+
+			manager.persist(new User("Jakab Gipsz"));
+			manager.persist(new User("Captain Nemo"));
+
+		}
+	}
+
+	private void listUsers() {
+		List<User> resultList = manager.createQuery("Select a From Employee a", User.class).getResultList();
+		System.out.println("num of Users:" + resultList.size());
+		for (User next : resultList) {
+			System.out.println("next user: " + next);
+		}
+	}
 }
+
